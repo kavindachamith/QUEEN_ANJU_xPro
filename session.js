7ba0b8b5d8562840a0d728de73d61412c812a726
paradox <136229133+kavindachamith@users.noreply.github.com>
2025-02-18 22:30:33 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~yYEHEaII#MiY0rlPFuiDr-moJxDDY3sXJJ4MBHeAqULcGYtPyruk",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~PH4TEawT#4omUIU3DJEqhjHbqx56U0X7SKBiHQx1reIjRrxtIKps",
 PORT: process.env.PORT || "8000"
 };
